8dcd2b1ec5f14d5999cf64b8b05026c69c75cecb
Shane <shane.swayney@gmail.com>
2023-07-30 16:05:27 -0600

    More edits and test files

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function() {
-    window.bookmarkver = '1.4';
+    window.bookmarkver = '0.0.1';
     const siteName = 'paycomonline.net';
     const pageName = 'Dashboard';
     const pageNameDisplay = 'Document Management Dashboard';
@@ -7,7 +7,18 @@ javascript: (function() {
     var isCorrectPage = document.location.href.includes(pageName);
     if (isCorrectSite && isCorrectPage) {
         alert('Init Miranda Document Downloader.');
-        console.log('Init Miranda Document Downloader...')
+        console.log('Init Miranda Document Downloader...');
+        var cachBustUrl = 'https://raw.githubusercontent.com/sswayney/Miranda/dev/miranda.js?' + (new Date().getDate());
+        fetch(cachBustUrl).then(function(response) {
+            return response.text();
+        }).then(function(data) {
+            var script = document.createElement('script');
+            script.id = 'mdd-script';
+            script.innerHTML = data;
+            document.getElementsByTagName('head')[0].appendChild(script);
+        }).catch(function() {
+            alert('Error retrieving miranda from github');
+        });
 
     } else if (confirm(`This script can only be run from your own user ${pageNameDisplay} on ${siteName}. Would you like to go there now?`)) {
         document.location = 'https://www.paycomonline.net/v4/cl/web.php/Doc/Dashboard';
@@ -17,14 +28,3 @@ javascript: (function() {
 })();
 
 
-var cachBustUrl = 'https://raw.githubusercontent.com/j0be/PowerDeleteSuite/master/powerdeletesuite.js?' + (new Date().getDate());
-fetch(cachBustUrl).then(function(response) {
-    return response.text();
-}).then(function(data) {
-    var script = document.createElement('script');
-    script.id = 'pd-script';
-    script.innerHTML = data;
-    document.getElementsByTagName('head')[0].appendChild(script);
-}).catch(function() {
-    alert('Error retreiving PowerDeleteSuite from github');
-});
