86200e40b3dcb65c0eae3ba761f95bd0f2297784
Shane <shane.swayney@gmail.com>
2023-08-20 15:15:48 -0600

    5 at a time.

--- app/buffered-request.ts
+++ app/buffered-request.ts
@@ -3,12 +3,18 @@ import {DataFilename} from "./types";
 const Buffer = require('buffer/').Buffer
 
 
-const makeRequest = async (url: string, axiosClient) => {
+/**
+ * Makes a request to fill out data in the DataFilename and returns a
+ * copy of the DataFilename with the data filled out.
+ * @param df
+ * @param axiosClient
+ */
+const makeRequest = async (df: DataFilename, axiosClient): Promise<DataFilename> => {
     try {
-        const response = await axiosClient.get(url);
-        return response.data;
+        const response = await axiosClient.get(df.downloadUrl);
+        return ({data: response.data, downloadUrl: df.downloadUrl, fileName: df.fileName});
     } catch (error) {
-        console.error(`Error fetching ${url}: ${error.message}`);
+        console.error(`Error fetching ${df.downloadUrl}: ${error.message}`);
         return null;
     }
 };
@@ -16,32 +22,34 @@ const makeRequest = async (url: string, axiosClient) => {
 export const makeBufferedRequests = async (axiosClient, dataFileNames: DataFilename[], bufferSize: number, logOutput: HTMLParagraphElement = null): Promise<DataFilename[]> => {
     const buffer: DataFilename[] = [];
     let bufferSizeCounter = 0;
+    const concurrentCount = 5;
 
-    for (const df of dataFileNames) {
-        console.log(`Downloading ${df.fileName}`);
+    for (let i = 0; i < dataFileNames.length; i += concurrentCount) {
+        console.log(`Downloading files...`);
         if (logOutput) {
-            logOutput.innerText = `Downloading ${df.fileName}`;
+            logOutput.innerText = `Downloading files...`;
         }
-        const data = await makeRequest(df.downloadUrl, axiosClient);
 
-        if (data !== null) {
-            bufferSizeCounter += Buffer.from(data).length;
+        const requests: DataFilename[] = dataFileNames.slice(i, i + concurrentCount);
+        const dataFileNamesResults = await Promise.all(requests.map(df => makeRequest(df, axiosClient)));
 
 
-            buffer.push({
-                fileName: df.fileName,
-                downloadUrl: df.downloadUrl,
-                data: data
-            });
-
-            if (bufferSizeCounter >= bufferSize) {
-                console.log(`Limit Reached`);
-                console.log(`Current buffer size: ${bufferSizeCounter}`);
-                console.log(`Buffer Size Limit: ${bufferSize}`);
-                break;
+        for(let df of dataFileNamesResults){
+            if (df !== null) {
+                bufferSizeCounter += Buffer.from(df.data).length;
+                buffer.push(df);
+                console.log(`Saving Files...`);
+                if (logOutput) {
+                    logOutput.innerText = `Saving files...`;
+                }
+                if (bufferSizeCounter >= bufferSize) {
+                    console.log(`Limit Reached`);
+                    console.log(`Current buffer size: ${bufferSizeCounter}`);
+                    console.log(`Buffer Size Limit: ${bufferSize}`);
+                    return buffer;
+                }
             }
         }
     }
     return buffer;
-
 };
